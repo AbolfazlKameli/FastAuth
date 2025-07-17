18f04ec8a768b8d4253381b2b5c5bbe5345afdbf
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-07-17 19:07:07 +0330

    fix: Made email handler sync for celery.

--- app/common/email/email_handler.py
+++ app/common/email/email_handler.py
@@ -1,19 +1,20 @@
 from email.message import EmailMessage
-
-from aiosmtplib import SMTP
+from smtplib import SMTP_SSL, SMTPException
 
 from app.configs.config import configs
 
 
 class EmailHandler:
-    def __init__(self):
-        self.smtp = SMTP(
-            hostname=configs.EMAIL_HOSTNAME,
-            port=configs.EMAIL_PORT,
-            username=configs.EMAIL_HOST_USERNAME,
-            password=configs.EMAIL_HOST_PASSWORD,
-            use_tls=configs.EMAIL_USE_TLS
-        )
+    @staticmethod
+    def _login(smtp):
+        try:
+            smtp.login(
+                user=configs.EMAIL_HOST_USERNAME,
+                password=configs.EMAIL_HOST_PASSWORD
+            )
+        except SMTPException:
+            pass
+            # add logger
 
     @staticmethod
     def _prepare_message(to: str, subject: str, body: str) -> EmailMessage:
@@ -24,7 +25,9 @@ class EmailHandler:
         msg.set_content(body)
         return msg
 
-    async def send_email(self, to: str, subject: str, body: str):
+    def send_email(self, to: str, subject: str, body: str):
         msg = self._prepare_message(to, subject, body)
-        async with self.smtp as smtp:
-            await smtp.send_message(msg)
+        smtp = SMTP_SSL(host=configs.EMAIL_HOSTNAME, port=configs.EMAIL_PORT)
+        with smtp as smtp:
+            self._login(smtp)
+            smtp.send_message(msg)
