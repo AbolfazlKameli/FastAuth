bbdb1df4e7a4fcd4c40b0b6ec5eacd9e6cfa94da
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-07-17 16:39:43 +0330

    feat: Add custom HTTPException handler.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, Request
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from .schemas import DataSchema
 
@@ -7,6 +9,15 @@ app = FastAPI(
     summary='A simple authentication application written in FastAPI',
     version='0.1.0',
 )
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: StarletteHTTPException):
+    return JSONResponse(
+        content={"data": {'errors': exc.detail}},
+        status_code=exc.status_code,
+        headers=exc.headers,
+    )
 
 
 @app.get('/health-check', status_code=status.HTTP_200_OK, response_model=DataSchema)
