18b913388d7895327b8fe81d60c82787330ac2cd
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-04 18:50:27 +0330

    feat: Add PaginationResponse schema.

--- app/schemas.py
+++ app/schemas.py
@@ -1,12 +1,19 @@
 from typing import Generic, TypeVar
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 DataT = TypeVar("DataT")
 
 
 class DataSchema(BaseModel, Generic[DataT]):
     data: DataT
+
+
+class PaginatedResponse(BaseModel, Generic[DataT]):
+    count: int = Field(description='Number of total items')
+    previous_page: int | None = Field(None, description='Previous page if it exists')
+    next_page: int | None = Field(None, description='Next page if it exists')
+    items: list[DataT] = Field(description='List of items returned in a paginated response')
 
 
 class HealthCheckResponse(BaseModel):
