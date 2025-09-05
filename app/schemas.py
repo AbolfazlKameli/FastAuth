8b46c6c49eda83606336699bf67ecd83e55b2ce7
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-05 22:46:23 +0330

    fix: Fix paginatedresponse schema validation.

--- app/schemas.py
+++ app/schemas.py
@@ -11,8 +11,8 @@ class DataSchema(BaseModel, Generic[DataT]):
 
 class PaginatedResponse(BaseModel, Generic[DataT]):
     count: int = Field(description='Number of total items')
-    previous_page: int | None = Field(None, description='Previous page if it exists')
-    next_page: int | None = Field(None, description='Next page if it exists')
+    previous_page: int | None = Field(None, description='Previous page if it exists', ge=1)
+    next_page: int | None = Field(None, description='Next page if it exists', ge=1)
     items: list[DataT] = Field(description='List of items returned in a paginated response')
 
 
