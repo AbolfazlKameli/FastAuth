b991f807aaacfe082db22e8809cdce79d0b6495d
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-05 16:54:55 +0330

    fix: Fix per_page default value, change response model.

--- app/api/users/routers.py
+++ app/api/users/routers.py
@@ -2,11 +2,11 @@ from typing import Annotated
 
 from fastapi import APIRouter, status, Query
 
+from app.core.pagination import paginate
 from app.dependencies import db_dependency
-from app.schemas import PaginatedResponse
+from app.schemas import PaginatedResponse, DataSchema
 from .repository import get_all_users
 from .schemas import UserBase
-from ...core.pagination import paginate
 
 router = APIRouter(
     prefix='/users',
@@ -14,10 +14,10 @@ router = APIRouter(
 )
 
 
-@router.get("", response_model=PaginatedResponse[UserBase], status_code=status.HTTP_200_OK)
+@router.get("", response_model=DataSchema[PaginatedResponse[UserBase]], status_code=status.HTTP_200_OK)
 async def list_users(
         db: db_dependency,
         page: Annotated[int, Query(ge=1)] = 1,
-        per_page: Annotated[int, Query(ge=1, le=100)] = 1,
+        per_page: Annotated[int, Query(ge=1, le=100)] = 10,
 ):
-    return await paginate(get_all_users(), db, page, per_page)
+    return DataSchema(data=await paginate(get_all_users(), db, page, per_page))
