deaaef3b4015987a63e299de908cae0f83e30032
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-05 17:03:41 +0330

    feat: Add user profile API.

--- app/api/users/routers.py
+++ app/api/users/routers.py
@@ -1,11 +1,11 @@
 from typing import Annotated
 
-from fastapi import APIRouter, status, Query
+from fastapi import APIRouter, status, Query, Path
 
 from app.core.pagination import paginate
 from app.dependencies import db_dependency
 from app.schemas import PaginatedResponse, DataSchema
-from .repository import get_all_users
+from .repository import get_all_users, get_user_by_id
 from .schemas import UserBase
 
 router = APIRouter(
@@ -21,3 +21,9 @@ async def list_users(
         per_page: Annotated[int, Query(ge=1, le=100)] = 10,
 ):
     return DataSchema(data=await paginate(get_all_users(), db, page, per_page))
+
+
+@router.get("/{user_id}", response_model=DataSchema[UserBase], status_code=status.HTTP_200_OK)
+async def get_user(db: db_dependency, user_id: Annotated[int, Path(ge=1)]):
+    user = await get_user_by_id(db, user_id)
+    return DataSchema(data=user)
