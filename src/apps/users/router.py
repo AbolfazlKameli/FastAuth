a80800786f5bfeb5b1dcf40bc0cd9500955f836f
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-18 11:29:56 +0330

    fix: Add service layer and exception handling.

--- src/apps/users/router.py
+++ src/apps/users/router.py
@@ -2,11 +2,10 @@ from typing import Annotated
 
 from fastapi import APIRouter, status, Query, Path
 
-from src.core.pagination import paginate
 from src.core.schemas import PaginatedResponse, DataSchema
 from src.dependencies import db_dependency
-from .repository import get_all_users, get_user_by_id
 from .schemas import UserOut
+from .services import get_all_users_paginated, get_user_or_404
 
 router = APIRouter(
     prefix='/users',
@@ -20,10 +19,10 @@ async def list_users(
         page: Annotated[int, Query(ge=1)] = 1,
         per_page: Annotated[int, Query(ge=1, le=100)] = 10,
 ):
-    return DataSchema(data=await paginate(get_all_users(), db, page, per_page))
+    return DataSchema(data=await get_all_users_paginated(db, page, per_page))
 
 
 @router.get("/{user_id}", response_model=DataSchema[UserOut], status_code=status.HTTP_200_OK)
 async def get_user(db: db_dependency, user_id: Annotated[int, Path(ge=1)]):
-    user = await get_user_by_id(db, user_id)
+    user = await get_user_or_404(db, user_id)
     return DataSchema(data=user)
