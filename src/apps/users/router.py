316be31c4f697d202953739a8a756e5edfa8c55e
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-18 11:45:48 +0330

    feat: Add additional not found response.

--- src/apps/users/router.py
+++ src/apps/users/router.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from fastapi import APIRouter, status, Query, Path
 
-from src.core.schemas import PaginatedResponse, DataSchema
+from src.core.schemas import PaginatedResponse, DataSchema, NotFoundResponse
 from src.dependencies import db_dependency
 from .schemas import UserOut
 from .services import get_all_users_paginated, get_user_or_404
@@ -22,7 +22,16 @@ async def list_users(
     return DataSchema(data=await get_all_users_paginated(db, page, per_page))
 
 
-@router.get("/{user_id}", response_model=DataSchema[UserOut], status_code=status.HTTP_200_OK)
+@router.get(
+    "/{user_id}",
+    response_model=DataSchema[UserOut],
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "model": DataSchema[NotFoundResponse],
+        },
+    }
+)
 async def get_user(db: db_dependency, user_id: Annotated[int, Path(ge=1)]):
     user = await get_user_or_404(db, user_id)
     return DataSchema(data=user)
