f20aac2cbeff83833519bf3017ad223930191d7b
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-21 21:58:04 +0330

    feat: Add user register schemas.

--- src/apps/auth/schemas.py
+++ src/apps/auth/schemas.py
@@ -1,4 +1,9 @@
-from pydantic import BaseModel, EmailStr
+from typing import Self
+
+from pydantic import BaseModel, EmailStr, Field, model_validator
+
+from src.apps.users.schemas import UserOut
+from .validators import PasswordValidator
 
 
 class UserRegisterRequest(BaseModel):
@@ -12,4 +17,18 @@ class UserOTPResponse(BaseModel):
 
 class UserRegisterResponse(BaseModel):
     message: str
-    otp_valid_time: str
+    user: UserOut
+
+
+class OtpVerifyUserRegisterRequest(BaseModel):
+    email: EmailStr
+    username: str = Field(min_length=4)
+    password: PasswordValidator
+    confirm_password: PasswordValidator
+    otp_code: str = Field(min_length=6, max_length=6)
+
+    @model_validator(mode="after")
+    def check_passwords_match(self) -> Self:
+        if self.password != self.confirm_password:
+            raise ValueError("Passwords don't match")
+        return self
