856ada602a7976d94314d13ab9c8abca5dd8f746
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-21 21:59:21 +0330

    feat: Add a query to check existing emails and usernames.

--- src/apps/users/repository.py
+++ src/apps/users/repository.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, exists
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .models import User
@@ -16,3 +16,13 @@ async def get_user_by_id(db: AsyncSession, user_id: int):
 async def get_user_by_email(db: AsyncSession, email: str) -> User | None:
     stmt = await db.execute(select(User).where(User.email == email))
     return stmt.scalar_one_or_none()
+
+
+async def user_exists_with_email_or_username(db: AsyncSession, email: str, username: str) -> bool:
+    email_stmt = await db.execute(select(exists(User)).where(User.email == email))
+    email_result = email_stmt.scalar()
+
+    username_stmt = await db.execute(select(exists(User)).where(User.username == username))
+    username_result = username_stmt.scalar()
+
+    return email_result or username_result
