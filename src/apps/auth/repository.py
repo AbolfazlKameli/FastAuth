3fd5437c180ba70ba7e92c3311657c1175fefe9d
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-21 21:58:41 +0330

    feat: Add a query to find an OTP by email.

--- src/apps/auth/repository.py
+++ src/apps/auth/repository.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from .models import OtpBlacklist
+from .models import OtpBlacklist, Otp
 
 
 async def get_active_blacklist_by_email(db: AsyncSession, email: str, current_time: datetime) -> OtpBlacklist | None:
@@ -17,5 +17,11 @@ async def get_active_blacklist_by_email(db: AsyncSession, email: str, current_ti
             )
         )
     )
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()
+
+
+async def get_otp_by_email(db: AsyncSession, email: str) -> Otp | None:
+    stmt = select(Otp).where(Otp.email == email)
     result = await db.execute(stmt)
     return result.scalar_one_or_none()
