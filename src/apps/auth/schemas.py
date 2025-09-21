289f82fa843bf3f7d473525582472c7061c9ae5f
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-21 21:57:11 +0330

    feat: Add a response schema for otp request endpoint.

--- src/apps/auth/schemas.py
+++ src/apps/auth/schemas.py
@@ -5,6 +5,11 @@ class UserRegisterRequest(BaseModel):
     email: EmailStr
 
 
+class UserOTPResponse(BaseModel):
+    message: str
+    otp_valid_time: str
+
+
 class UserRegisterResponse(BaseModel):
     message: str
     otp_valid_time: str
