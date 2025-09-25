0a0e4003ee81cd51b7de2880c57f60d32e27244b
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-09-25 12:41:15 +0330

    feat: Add fastapi cache.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,36 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, status, Request
 from fastapi.responses import JSONResponse
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from src.apps.auth.router import router as auth_router
 from src.apps.users.router import router as user_router
 from src.core.configs.logging_config import setup_logging
 from src.core.schemas import DataSchema, HealthCheckResponse
+from src.infrastructure.redis_pool import redis_fastapi
 
 setup_logging()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    backend = RedisBackend(redis_fastapi)
+    FastAPICache().init(backend=backend, prefix="FastAuth_")
+
+    yield
+
+    await redis_fastapi.close()
+    await redis_fastapi.connection_pool.disconnect()
+
 
 app = FastAPI(
     title='FastAuth',
     summary='A simple authentication application written in FastAPI',
     version='0.2.0',
+    lifespan=lifespan,
 )
 
 app.include_router(user_router)
