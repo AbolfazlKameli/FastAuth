672dc630aa9d97d048ab4b02c534eafe83ad3ae1
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-10-06 20:44:40 +0330

    hotfix: Add retry for send otp task.

--- src/apps/tasks.py
+++ src/apps/tasks.py
@@ -2,8 +2,11 @@ from src.infrastructure.celery_worker.celery_app import celery_app
 from src.infrastructure.email_handler import EmailHandler
 
 
-@celery_app.task
-def send_otp_code_email(email: str, otp_code: str):
+@celery_app.task(bind=True)
+def send_otp_code_email(self, email: str, otp_code: str):
     content = f"Your verification code is {otp_code}"
     subject = "Welcome to FastAuth"
-    EmailHandler().send_email(email, subject, content)
+    try:
+        EmailHandler().send_email(email, subject, content)
+    except Exception as e:
+        self.retry(exc=e, max_retries=3, countdown=10)
