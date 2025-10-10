4735c309344a29f9646aded7d6768a3b7bbcbaa0
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-10-10 17:36:19 +0330

    docs: change version.

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title='FastAuth',
     summary='A simple authentication application written in FastAPI',
-    version='0.3.0',
+    version='0.4.0',
     lifespan=lifespan,
     contact={
         "name": "Denver",
